97ecc5a46a5443011ae895f03808b8dbf631b8ee
Quentin Weber <webquentin@web.de>
2019-03-20 21:34:33 +0100

    Further adjustments for RPI

--- vagrant/startupRPI.sh
+++ vagrant/startupRPI.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
+
+# prepare PI
+echo "------------"
+echo " prepare PI "
+echo "------------"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install -y mc zip unzip
+
+# prepare node.js
+echo "-----------------"
+echo " prepare Node.js "
+echo "-----------------"
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt install -y nodejs
 
 sudo npm install yarn pm2 pm2-logrotate -g
 
-GRANT ALL PRIVILEGES ON *.* TO 'service'@'localhost' IDENTIFIED BY 'service';
+# prepare node.js
+echo "-----------------"
+echo " prepare MySQL "
+echo "-----------------"
+sudo apt install -y mysql-server
+
+echo "CREATE DATABASE moneymaster" | sudo mysql -uroot -proot
+echo "GRANT ALL PRIVILEGES ON *.* TO 'service'@'localhost' IDENTIFIED BY 'service';" | sudo mysql -uroot -proot
+
