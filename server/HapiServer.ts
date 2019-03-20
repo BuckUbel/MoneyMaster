4776facb0ddfc762ed54d576181b8f26386f8aac
Quentin Weber <webquentin@web.de>
2019-03-20 23:28:16 +0100

    Rebuild on hapi-server

---
+++ server/HapiServer.ts
@@ -0,0 +1,28 @@
+import * as Hapi from "hapi";
+import {IDatabase} from "./database";
+
+export interface IServerConfig {
+  prefix: string;
+  port: number;
+  publicDir: string;
+  entryFile: string;
+  downloadPath: string;
+}
+
+export interface IHapiServer {
+  app: Hapi.Server;
+  database: IDatabase;
+  serverConfig: IServerConfig;
+}
+
+export class HapiServer implements IHapiServer {
+  public app: Hapi.Server;
+  public database: IDatabase;
+  public serverConfig: IServerConfig;
+
+  public constructor(server: Hapi.Server, database: IDatabase, serverConfig: IServerConfig){
+    this.app = server;
+    this.database = database;
+    this.serverConfig = serverConfig;
+  }
+}
--- server/routes.ts
+++ server/routes.ts
@@ -1,97 +1,73 @@
-import asyncHandler from "express-async-handler";
-import {IExpressServer} from "./expressServer";
 import {
-  IBookingsObjectProps,
-  IBookingsObjectPropsFromRequest,
   insertABooking,
   loadAllBookingsFromDB,
-  updateABooking
 } from "./database/model/Bookings";
+import * as Hapi from "hapi";
 import {getCSVDataFromSPKBLK} from "./puppeteer/SPK-BLK";
-import fs, {Stats} from "fs";
-import {promisify} from "util";
-import {arrayToBookingModel, BookingModel} from "./database/model/BookingModel";
-
-const readdir = promisify(fs.readdir);
-const stat = promisify(fs.stat);
-const readFile = promisify(fs.readFile);
-
-interface IFileStats {
-  stats: Stats;
-  path: string;
-}
-
-async function getLastModifiedFileInDir(dir: string) {
-  const filePathList = await readdir(dir);
-  const fileObjectList: Stats[] = await Promise.all(filePathList.map(async (file: string) => {
-    const path = dir + "/" + file;
-    return await stat(path);
-  }));
-
-  const fileObjectListWithPaths: IFileStats[] = filePathList.map((filePath: string, index: number): IFileStats => {
-    return {
-      stats: fileObjectList[index],
-      path: dir + "/" + filePath,
-    };
-  });
-
-  fileObjectListWithPaths.sort((a: IFileStats, b: IFileStats): number => {
-    return b.stats.ctimeMs - a.stats.ctimeMs;
-  });
-
-  return fileObjectListWithPaths[0].path;
-}
-
-export function getStringValues(element: string, separator: string): string[] {
-  return element.split(separator).map((p: string): string => {
-    return p ? p.replace(/[\"]/gi, "") : "";
-  });
-}
-
-interface IDifferentExportedBookings {
-  addBookings: BookingModel[];
-  editBookings: BookingModel[];
-}
-
-function extractBookingsFromFile(fileContent: string, bookings: BookingModel[]): IDifferentExportedBookings {
-  const elements: string[] = fileContent.split("\n");
-  elements.pop();
-  const categories = getStringValues(elements[0], ";");
-  elements.shift();
-
-  const newBookings = elements.map((el: string): BookingModel => {
-    return arrayToBookingModel(getStringValues(el, ";"), categories);
-  });
-
-  const updateBookings: BookingModel[] = [];
-  const reallyNewBookings = newBookings.filter((newBooking) => {
-
-    return bookings.every((oldBooking: BookingModel) => {
-      const bookingStatus = newBooking.equals(oldBooking);
-      if (bookingStatus === "ignore") {
-        return false;
-      }
-      if (bookingStatus === "update") {
-        updateBookings.push(newBooking);
-        return false;
-      }
-      return true;
-    });
-  });
-  return {
-    addBookings: reallyNewBookings,
-    editBookings: updateBookings
-  };
-  // this.setState({upload: reallyNewBookings});
-}
+import {IHapiServer} from "./HapiServer";
+import {extractBookingsFromFile, getLastModifiedFileInDir, IDifferentExportedBookings, readFile} from "./helper";
 
 export default class ServerRoutes {
 
-  public server: IExpressServer;
+  public server: IHapiServer;
 
-  public constructor(server: IExpressServer) {
+  public constructor(server: IHapiServer) {
     this.server = server;
-    this.init();
+  }
+
+  public staticFileRouting() {
+    this.server.app.route({
+      method: "GET",
+      path: "/{any*}",
+      handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+        return h.file(this.server.serverConfig.entryFile);
+      }
+    });
+    this.server.app.route({
+      method: "GET",
+      path: "/public/{param*}",
+      handler: {
+        directory: {
+          path:  this.server.serverConfig.publicDir
+        }
+      }
+    });
+  }
+
+  public async loadDataFromSPKBLK() {
+    let result: IDifferentExportedBookings = {
+      addBookings: [],
+      editBookings: [],
+    };
+    try {
+      await getCSVDataFromSPKBLK(this.server.serverConfig.downloadPath);
+      console.log("CSV is downloaded.");
+    } catch (e) {
+      console.log("CSV is false downloaded.");
+    }
+    try {
+      const file = await getLastModifiedFileInDir(this.server.serverConfig.downloadPath);
+      result = await this.updateDatabaseBookings(file);
+      console.log("CSV was read successfully:");
+      console.log("Items to add:" + result.addBookings.length);
+      console.log("Items to edit:" + result.editBookings.length);
+    } catch (e) {
+      console.log("CSV was not read.");
+      console.log(e);
+    }
+    try {
+      if (result.addBookings.length > 0) {
+        await insertABooking(this.server.database, result.addBookings);
+      }
+      // if (result.editBookings.length > 0) {
+      // await updateABooking(this.server.database, result.editBookings);
+      // }
+
+      console.log("Bookings are imported.");
+    } catch (e) {
+      console.log("Bookings are not imported:");
+      console.log(e);
+    }
   }
 
   public async updateDatabaseBookings(file: string) {
@@ -99,81 +75,54 @@ export default class ServerRoutes {
     const fileContent = await readFile(file, "binary");
     return extractBookingsFromFile(fileContent, rows);
     // return await updateABooking(this.server.database, result.editBookings);
-
   }
 
-  public init() {
-    this.server.app.get("/api/bookings/load", asyncHandler(async (req, res) => {
-      const rows = await loadAllBookingsFromDB(this.server.database)
-        .then((result) => {
-          return result;
-        }).catch((error) => {
-          return error;
-        });
-      res.json(rows);
-    }));
-    // this.server.app.post("/api/bookings/add", asyncHandler(async (req, res) => {
-    //   const objects: IBookingsObjectPropsFromRequest[] = req.body.element;
-    //   const rows = await insertABooking(this.server.database, objects)
-    //     .then((result) => {
-    //       return result;
-    //     }).catch((error) => {
-    //       return error;
-    //     });
-    //   res.json(rows);
-    // }));
-    this.server.app.post("/api/bookings/edit", asyncHandler(async (req, res) => {
-      const object: IBookingsObjectProps = req.body;
-      console.log(object);
-      const rows = await updateABooking(this.server.database, object)
-        .then((result) => {
-          return result;
-        }).catch((error) => {
-          return error;
-        });
-      res.json(rows);
-    }));
+  public async init() {
 
-    this.server.app.get("/api/bookings/loadFromSPK", asyncHandler(async (req, res) => {
-        res.send("Data will imported and will save in the database");
+    await this.server.app.register(require("inert"));
+    this.staticFileRouting();
 
-        let result: IDifferentExportedBookings = {
-          addBookings: [],
-          editBookings: [],
-        };
-        try {
-          await getCSVDataFromSPKBLK(this.server.serverConfig.downloadPath);
-          console.log("CSV is downloaded.");
-        } catch (e) {
-          console.log("CSV is false downloaded.");
-        }
-        try {
-          const file = await getLastModifiedFileInDir(this.server.serverConfig.downloadPath);
-          result = await this.updateDatabaseBookings(file);
-          console.log("CSV was read successfully:");
-          console.log("Items to add:" + result.addBookings.length);
-          console.log("Items to edit:" + result.editBookings.length);
-        } catch (e) {
-          console.log("CSV was not read.");
-          console.log(e);
-        }
-        try {
-          if (result.addBookings.length > 0) {
-            await insertABooking(this.server.database, result.addBookings);
-          }
-          // if (result.editBookings.length > 0) {
-          // await updateABooking(this.server.database, result.editBookings);
-          // }
-
-          console.log("Bookings are imported.");
-        } catch (e) {
-          console.log("Bookings are not imported:");
-          console.log(e);
-        }
+    this.server.app.route({
+      method: "GET",
+      path: "/api/bookings/load",
+      handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+        const rows = loadAllBookingsFromDB(this.server.database)
+          .then((result) => {
+            return result;
+          }).catch((error) => {
+            return error;
+          });
+        return rows;
       }
-    ));
-    this.server.app.get("*", (req, res) => {
-      res.sendFile(this.server.serverConfig.entryFile);
+    });
+
+    this.server.app.route({
+      method: "POST",
+      path: "/api/bookings/edit",
+      handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+        // TODO: Überarbeiten
+        /*
+        const object: IBookingsObjectProps = request.params.body;
+                console.log(object);
+                const rows = updateABooking(this.server.database, object)
+                  .then((result) => {
+                    return result;
+                  }).catch((error) => {
+                    return error;
+                  });
+                return rows;
+                */
+        return null;
+      }
+    });
+
+    this.server.app.route({
+      method: "GET",
+      path: "/api/bookings/loadFromSPK",
+      handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+        this.loadDataFromSPKBLK();
+        return "Data will imported and will save in the database";
+      }
     });
   }
 }
