5bafa546d4e0a186b4aa51e6eb7b0dd1c8e5ed42
Quentin Weber <webquentin@web.de>
2019-03-20 21:38:28 +0100

    Further adjustments for RPI

--- vagrant/startupRPI.sh
+++ vagrant/startupRPI.sh
@@ -8,6 +8,14 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install -y mc zip unzip
 
+# prepare mysql
+echo "-----------------"
+echo " prepare MySQL "
+echo "-----------------"
+sudo apt install -y mysql-server
+echo "CREATE DATABASE moneymaster" | sudo mysql -uroot -proot
+echo "GRANT ALL PRIVILEGES ON *.* TO 'service'@'localhost' IDENTIFIED BY 'service';" | sudo mysql -uroot -proot
+
 # prepare node.js
 echo "-----------------"
 echo " prepare Node.js "
@@ -16,13 +24,9 @@ curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt install -y nodejs
 
 sudo npm install yarn pm2 pm2-logrotate -g
+cd /home/pi/MoneyMaster
+sudo yarn install -y
 
-# prepare node.js
-echo "-----------------"
-echo " prepare MySQL "
-echo "-----------------"
-sudo apt install -y mysql-server
 
-echo "CREATE DATABASE moneymaster" | sudo mysql -uroot -proot
-echo "GRANT ALL PRIVILEGES ON *.* TO 'service'@'localhost' IDENTIFIED BY 'service';" | sudo mysql -uroot -proot
-
+#Import Database
+sudo mysql -uservice -pservice moneymaster vagrant/dump.sql
