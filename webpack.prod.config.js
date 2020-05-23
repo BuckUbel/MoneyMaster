8550d700323423f6e312a3b07e375cd6d625b831
BuckUbel <webquentin@web.de>
2020-05-23 23:43:06 +0200

    Updated prod webpack file

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,66 +1,74 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    entry: './client_src/react/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: true }
-                    }
-                ]
-            },
+  entry: './client_src/react/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: [
+          /server/
+        ],
+        options: {
+          configFile: "tsconfig.client.json",
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: [
+          /server/
+        ],
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: [
+          /server/
+        ],
+        use: ['file-loader']
+      },
+      {
+        test: /\.html$/,
+        exclude: [
+          /server/
+        ],
+        use: [
+          {
+            loader: "html-loader",
+            options: {minimize: true}
+          }
         ]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    output: {
-        path: path.join(__dirname, 'public'),
-        publicPath: '/',
-        filename: '[projectName].js'
-    },
-    mode: 'production',
-    target: 'web',
-    devtool: '#source-map',
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true // set to true if you want JS source maps
-            }),
-            new OptimizeCSSAssetsPlugin({})
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './client_src/html/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[projectName].css',
-            chunkFilename: '[id].css'
-        })
+      },
     ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: 'main.js'
+  },
+  mode: 'production',
+  target: 'web',
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./client_src/html/index.html",
+      filename: "./index.html",
+      excludeChunks: ['/server/']
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'moneymaster.css',
+      chunkFilename: '[id].css'
+    })
+  ]
 };
